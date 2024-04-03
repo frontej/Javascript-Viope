beb0270967ce1d99e888896e240b593c7eaf2d73
frontej <165875930+frontej@users.noreply.github.com>
2024-04-03 11:55:58 +0300

    3.1

---
+++ Teht_3.1.js
@@ -0,0 +1,5 @@
+function func(){
+  console.log(document.getElementById("teksti").innerHTML);
+  document.getElementById("teksti").innerHTML ="JavaScript on helppoa ja hauskaa!";
+  console.log(document.getElementById("teksti").innerHTML);
+}
