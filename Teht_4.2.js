1115a0844ef257a2374c112779b0f4c70099ef4a
frontej <165875930+frontej@users.noreply.github.com>
2024-04-03 11:59:18 +0300

    4.2

---
+++ Teht_4.2.js
@@ -0,0 +1,5 @@
+function laskeNelio() {
+	var arvo = document.getElementById("luku").value;
+	var tulos = arvo * arvo;
+	document.getElementById("tulos").innerHTML = "Luvun " + arvo + " neliö on " + tulos;
+};
