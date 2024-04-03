49657bba46c521588c6cb9da509c21cdbea3b931
frontej <165875930+frontej@users.noreply.github.com>
2024-04-03 11:53:45 +0300

    2.3

---
+++ Teht_2.3.js
@@ -0,0 +1,22 @@
+var kerta=0
+
+var laskin=0
+var luku,luku_2 
+for(luku_2=2;luku_2<=60;luku_2++)
+{
+for( luku=1;luku<=luku_2;luku++)
+{
+  if(luku_2%luku==0)
+  laskin++
+
+  
+}
+
+if(laskin==2)
+
+console.log(luku_2) + kerta++;
+laskin=0
+
+}
+
+console.log("Alkulukuja löydetty yhteensä: "+ kerta)
